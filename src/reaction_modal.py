30f27c7e1af81515c8c9c20a4784f11a32c52dba
Golf0ned <benjaminye.email@gmail.com>
2025-03-04 23:04:55 -0600

    minor bug fixes

--- src/reaction_modal.py
+++ src/reaction_modal.py
@@ -16,27 +16,26 @@ class ReactionModal(discord.ui.Modal):
         super().__init__(title="Add Reaction")
         self.message = message
 
-        self.add_item(discord.ui.InputText(label="Reaction (alphanumeric with spaces)"))
+        self.add_item(discord.ui.InputText(label="Reaction (A-Z 0-9)", ))
 
     async def callback(self, interaction: discord.Interaction):
         await interaction.response.defer()
-        input = self.children[0].value
+        input = self.children[0].value.lower()
 
         # Check if input is valid
         if not input or not input.isalnum():
-            await interaction.followup.send("Invalid input. Please provide an alphanumeric string with no spaces.")
+            await interaction.followup.send("Invalid input. Please provide an alphanumeric string with no spaces.", ephemeral=True)
             return
 
         # Make sure there's enough emojis to react with
         input_count = Counter(input)
         for c in input_count:
             if input_count[c] > EMOJI_COUNTS[c]:
-                await interaction.followup.send(f"Not enough emojis for {c} (There's {EMOJI_COUNTS[c]} {c}'s).")
+                await interaction.followup.send(f"Not enough emojis for {c} (There's {EMOJI_COUNTS[c]} {c}'s).", ephemeral=True)
                 return
 
         # All clear, add reactions
         cur_count = {}
-        for c in modal_input:
-            cur_emoji_num = cur_count.get(c, 0) + 1 # 1 indexed lmao
-            await self.message.add_reaction()
-            cur_count[c] = cur_emoji_num
+        for c in input:
+            await self.message.add_reaction(EMOJIS[c][cur_count.get(c, 0)])
+            cur_count[c] = cur_count.get(c, 0) + 1
